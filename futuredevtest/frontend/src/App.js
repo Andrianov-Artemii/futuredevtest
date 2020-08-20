db6f261a0b4824b75f41c2bfaef3b2e347dff0dd
Artemy Andrianov <andrianovartemii@gmail.com>
2020-08-21 01:13:48 +0300

    Add react

--- futuredevtest/frontend/src/App.js
+++ futuredevtest/frontend/src/App.js
@@ -1,26 +1,30 @@
+import { render } from "react-dom";
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(props){
+
+  function componentDidMount() {
+    fetch("api/lead")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return "Something went wrong!";
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+    return (
+      <div>{console.log(componentDidMount())}</div>
+    );
 }
-
-export default App;
